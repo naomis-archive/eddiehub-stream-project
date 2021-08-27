6bff81b2e254ad48da793e03ac275d09a730c4f8
Eddie Jaoude <eddie@jaoudestudios.com>
2021-08-27 18:29:46 +0100

    feat: post endpoint

--- api/server.js
+++ api/server.js
@@ -4,9 +4,15 @@ const cors = require("cors");
 const port = 3000;
 
 app.use(cors({ origin: "*" }));
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send({ name: "hello world" });
+});
+
+app.post("/events", async (req, res) => {
+  console.log(req.body);
+  res.send({ id: "123" });
 });
 
 app.get("/events", (req, res) => {
@@ -17,6 +23,13 @@ app.get("/events", (req, res) => {
       eventDescription: "Event Description 1",
       eventDate: "Event Date 1",
       eventUrl: "Event Url 1",
+    },
+    {
+      name: "Person 2",
+      eventName: "Event Name 3",
+      eventDescription: "Event Description 2",
+      eventDate: "Event Date 2",
+      eventUrl: "Event Url 2",
     },
   ]);
 });
