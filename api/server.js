ba9a1aa3adce1bc252ccb5aece3aca57541151a1
Eddie Jaoude <eddie@jaoudestudios.com>
2021-08-27 18:00:32 +0100

    feat: events endpoint

--- api/server.js
+++ api/server.js
@@ -6,6 +6,18 @@ app.get("/", (req, res) => {
   res.send({ name: "hello world" });
 });
 
+app.get("/events", (req, res) => {
+  res.send([
+    {
+      name: "Person 1",
+      eventName: "Event Name 1",
+      eventDescription: "Event Description 1",
+      eventDate: "Event Date 1",
+      eventUrl: "Event Url 1",
+    },
+  ]);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
